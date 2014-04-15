24e8abac8d740200d11eeaaabdc7f6ca3d619357
Joakim Tømmer <joakimtommer@gmail.com>
2014-04-15 14:55:15 +0200

    Update InstrumentRegister.java

--- InstrumentRegister.java
+++ InstrumentRegister.java
@@ -1,3 +1,4 @@
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -17,7 +18,7 @@ public class InstrumentRegister
         first = null;
     }
 
-   /* public void insertInstrument(Instrument ny)
+    public void insertInstrument(Instrument ny)
     {
         if (ny == null)
             return;
@@ -35,7 +36,81 @@ public class InstrumentRegister
             helper.next = ny;
         }
     }
-    */
-}
+    public Instrument findID( String RegNum )
+    {
+        Instrument runner = first;
+       	if (runner != null && (runner.getRegNum().equalsIgnoreCase(RegNum)))
+            return runner;
+        else if (runner == null)
+            return null;
+        else
+	{
+            while ( runner.next != null && !((runner.next.getRegNum()).equalsIgnoreCase(RegNum)))
+            {
+                runner = runner.next;
+            }
+                        
+	}
+        return runner.next;
+    }
+    public Instrument findType( String type )
+    {
+        Instrument runner = first;
+       	if (runner != null && (runner.getType().equalsIgnoreCase(type)))
+            return runner;
+        else if (runner == null)
+            return null;
+        else
+	{
+            while ( runner.next != null && !((runner.next.getType()).equalsIgnoreCase(type)))
+            {
+                runner = runner.next;
+            }
+                        
+	}
+        return runner.next;
+    }
+    public Instrument findModel( String Model )
+    {
+        Instrument runner = first;
+       	if (runner != null && (runner.getModel().equalsIgnoreCase(Model)))
+            return runner;
+        else if (runner == null)
+            return null;
+        else
+	{
+            while ( runner.next != null && !((runner.next.getModel()).equalsIgnoreCase(Model)))
+            {
+                runner = runner.next;
+            }
+                        
+	}
+        return runner.next;
+    }
+
     
+    public String listEntries()
+	{
+                Instrument instrument;
+                
+		Instrument runner = first;
+		String info;
+		if(first != null)
+		{
+                        
+			info=first.toString() +"\n";
+			while ( runner.next != null)
+			{
+				runner = runner.next;
+				info += "\n" + runner.toString() +"\n";
+			}
+		}
+		else
+			info="Ingen Instrument i registeret!";
+		return info;
+	}
+}
+
    
+
+    
